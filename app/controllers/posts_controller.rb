a9f7f84f1b3abc88620d54e1f75708dcea1718ee
Second-wind <1073211217@qq.com>
2017-04-13 21:51:15 +0800

    use before_action to find_post_and_check_permission and use _form to simple the posts/edit/new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,12 @@ class PostsController < ApplicationController
 
   def edit
     @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
+    find_post_and_check_permission
   end
 
   def update
     @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
+    find_post_and_check_permission
 
     if @post.update(post_params)
       redirect_to account_posts_path, notice: "Update Success!"
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
   def destroy
     @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
+    find_post_and_check_permission
 
     @post.destroy
       redirect_to account_posts_path, alert: "Post Deleted!"
@@ -46,6 +46,14 @@ class PostsController < ApplicationController
 
 
   private
+
+  def find_post_and_check_permission
+    @post = Post.find(params[:id])
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission!"
+    end
+  end
 
   def post_params
     params.require(:post).permit(:content)
